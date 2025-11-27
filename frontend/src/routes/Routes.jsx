ec13eaba1dba58717efc10eac7910bf33cf69db3
abhiiinav199 <meshram.abhinav1998@gmail.com>
2025-11-27 18:03:18 +0530

    UI- Riding>jsx Completed

--- frontend/src/routes/Routes.jsx
+++ frontend/src/routes/Routes.jsx
@@ -10,6 +10,7 @@ import UserLogout from "../pages/UserLogout";
 import CaptainHome from "../pages/CaptainHome";
 import CaptainProtectedWrapper from "../pages/CaptainProtectedWrapper";
 import CaptainLogout from "../pages/CaptainLogout";
+import Riding from "../pages/Riding";
 
 
 const Routing = () => {
@@ -19,13 +20,14 @@ const Routing = () => {
                 <Route path="/" element={<Start />} />
                 <Route path="/signup" element={<UserSignup />} />
                 <Route path="/login" element={<UserLogin />} />
+                    <Route path="/riding" element={<Riding/>} />
                 <Route path="/captain-signup" element={<CaptainSignUp />} />
                 <Route path="/captain-login" element={<CaptainLogin />} />
                 <Route path="/home" element={
-                    // <UserProtectedWrapper>
+                    <UserProtectedWrapper>
                         <Home />
-                        }/>
-                    {/* </UserProtectedWrapper>} /> */}
+                        {/* // }/> */}
+                    </UserProtectedWrapper>} />
                 <Route path="/user/logout" element={
                     <UserProtectedWrapper>  
                         <UserLogout />
@@ -44,6 +46,8 @@ const Routing = () => {
                         <CaptainLogout/>
                     </CaptainProtectedWrapper>
                 } />
+
+            
             </Routes>
         </div>
     )
